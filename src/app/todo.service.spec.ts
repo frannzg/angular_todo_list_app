4b495463891f1254fd123d3d0b6b3ad654a762eb
frannzg <148803161+frannzg@users.noreply.github.com>
2025-03-06 10:32:51 +0100

    Se crean dos componentes, el de crear tarea y finalizar tarea. Se conecta el de crear tarea con la API de Spring Boot, pendiente realizar validaciones en frontal.

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
-import { TodoService } from './services/todo.service';
+
+import { TodoService } from './todo.service';
 
 describe('TodoService', () => {
   let service: TodoService;
