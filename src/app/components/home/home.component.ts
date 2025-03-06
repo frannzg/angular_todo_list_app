3cd983982534c2dd115a23d2a593198920f95149
frannzg <148803161+frannzg@users.noreply.github.com>
2025-03-06 12:45:15 +0100

    Se añade la funcionalidad de eliminar tarea, editar estado de la tarea, estilos varios a la web.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,20 +11,39 @@ import { CreateTaskModalComponent } from '../create-task-modal/create-task-modal
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  tasks: any[] = [];
+  tasksPending: any[] = [];
+  tasksDone: any[] = [];
   showModal: boolean = false;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get<any>('http://127.0.0.1:8080/api/v1/tasks/all').subscribe({
+    let status = "Pending";
+    
+    this.http.get<any>('http://127.0.0.1:8080/api/v1/tasks/all/status', {
+      params: { status: status }
+    }).subscribe({
       next: (data) => {
-        this.tasks = data;
+        this.tasksPending = data;
       },
       error: (err) => {
         console.error('Error al hacer la petición:', err);
       }
     });
+
+    status = "Done";
+
+    this.http.get<any>('http://127.0.0.1:8080/api/v1/tasks/all/status', {
+      params: { status: status }
+    }).subscribe({
+      next: (data) => {
+        this.tasksDone = data;
+      },
+      error: (err) => {
+        console.error('Error al hacer la petición:', err);
+      }
+    });
+
   }
 
   toggleModal() {
@@ -37,4 +56,42 @@ export class HomeComponent implements OnInit {
     }
     this.toggleModal(); 
   }
+
+  updateTask(task: any) {
+    const updatedTask = { 
+      task: task.task,
+      description: task.description,
+      status: task.status === 'Pending' ? 'Done' : 'Pending'
+    };
+  
+    this.http.put<any>(`http://127.0.0.1:8080/api/v1/tasks/update/${task.id}`, updatedTask).subscribe({
+      next: (data) => {
+        console.log('Tarea actualizada:', data);
+        const updatedTaskIndex = this.tasksPending.findIndex(t => t.id === task.id);
+        if (updatedTaskIndex !== -1) {
+          this.tasksPending[updatedTaskIndex] = data;
+        }
+      },
+      error: (err) => {
+        console.error('Error al guardar la tarea:', err);
+      }
+    });
+
+    location.reload();
+  }
+
+  deleteTask(task: any) {
+    this.http.delete<any>(`http://127.0.0.1:8080/api/v1/tasks/delete/${task.id}`).subscribe({
+      next: (data) => {
+        console.log('Tarea eliminada:', data);
+        // Elimina la tarea del array de tareas pendientes
+        this.tasksPending = this.tasksPending.filter(t => t.id !== task.id);
+      },
+      error: (err) => {
+        console.error('Error al eliminar la tarea:', err);
+      }
+    });
+      location.reload();
+  }
+  
 }
